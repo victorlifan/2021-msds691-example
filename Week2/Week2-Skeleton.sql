1ac2750aca9161519ae43d14b8201ef6b8c27297
Fan <victorlifan@vip.qq.com>
2021-09-13 09:52:59 -0700

    Update Week2-Skeleton.sql

--- Week2/Week2-Skeleton.sql
+++ Week2/Week2-Skeleton.sql
@@ -29,12 +29,12 @@ INSERT INTO epa_site_location VALUES (60070008,	'Chico-East Avenue', 39.76168, -
 
 
 COPY epa_site_location 
-FROM '/Users/dwoodbridge/Class/2021_MSDS691/Example/Data/epa_site_location.csv'
+FROM '/Users/fanli/Desktop/classes/fall_1/691_01_relational_database/2021-msds691-example/Data/epa_site_location.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY epa_air_quality 
-FROM '/Users/dwoodbridge/Class/2021_MSDS691/Example/Data/epa_air_quality.csv'
+FROM '/Users/fanli/Desktop/classes/fall_1/691_01_relational_database/2021-msds691-example/Data/epa_air_quality.csv'
 DELIMITER ','
 CSV HEADER;
 
@@ -46,32 +46,48 @@ INSERT INTO epa_site_location VALUES (60070001,	'Central Marin', 38.0834, -122.7
 --------------------------------------------------------
 -- 1.A From epa_air_quality, return all the records where site_id is 60070008.
 --------------------------------------------------------
-
+select *
+from epa_air_quality
+where site_id = 60070008;
 
 --------------------------------------------------------
 -- 1.B From epa_air_quality, return date and daily_aqi_value where site_id is 60070008.
 --------------------------------------------------------
-
+select date, daily_aqi_value
+from epa_air_quality
+where site_id = 60070008;
 
 --------------------------------------------------------
 -- 1.C From epa_air_quality, return unique dates where site_id is 60070008.
 --------------------------------------------------------
-
+select distinct date
+from epa_air_quality
+where site_id = 60070008;
 
 --------------------------------------------------------
 -- 1.D From epa_air_quality, return unique dates where site_id is 60070008 ordered by date (ascending).
 --------------------------------------------------------
-
+select distinct date
+from epa_air_quality
+where site_id = 60070008
+order by date;
 
 --------------------------------------------------------
 --  1.E From epa_air_quality, return first 5 dates where site_id is 60070008 between 2020-05-01 and 2020-10-01 ordered by date.
 --------------------------------------------------------
-
+select date
+from epa_air_quality
+where site_id = 60070008 and date between '2020-05-01' and '2020-10-01'
+order by date;
 
 --------------------------------------------------------
 -- 1.F From epa_air_quality, return last 5 dates where site_id is 60070008 between 2020-05-01 and 2020-10-01 ordered by date.
 --------------------------------------------------------
-
+select date
+from epa_air_quality
+where date between '2020-05-01' and '2020-10-01'
+order by date desc
+limit 5;
 
 ------------------------------------------------------------
 -- Type Conversion (p.19)
@@ -79,8 +95,8 @@ INSERT INTO epa_site_location VALUES (60070001,	'Central Marin', 38.0834, -122.7
 SELECT TO_CHAR(TIMESTAMP '2021-04-20 17:31:12.66', 'HH12:MI:SS');
 SELECT TO_DATE('05 Dec 2000', 'DD Mon YYYY');
 SELECT TO_TIMESTAMP('05 Dec 2000', 'DD Mon YYYY');
-SELECT TO_CHAR(-125.8, '999D999S');
-SELECT TO_NUMBER('12,454.8-', '99G999D9S');
+SELECT TO_CHAR(-125.8, '999D999');
+SELECT TO_NUMBER('12,454.8-', 'S99G999D9');
 
 
 --------------------------------------------------------
@@ -118,7 +134,10 @@ SELECT NULL OR False;
 -- 		year, month, day : numeric
 -- 		pm10, aqi : real
 --------------------------------------------------------
-
+select site_id, extract(year from date):: NUMERIC as year, extract(month from date)::NUMERIC as month, extract(day from date)::NUMERIC as day, 
+daily_mean_pm10_concentration pm10, daily_aqi_value aqi
+from epa_air_quality
+order by site_id, year, month, day;
 
 --------------------------------------------------------
 -- String Operators p.27
@@ -135,7 +154,7 @@ SELECT LENGTH('xyz'),
 -- String Operators p.28	   
 SELECT 'MSDS691' LIKE 'MSDS%', 
 	   'MSDS691' SIMILAR TO 'MSDS%',
-	   'MSDS691' LIKE 'MSDS[0-9]+',
+	   'MSDS691' LIKE 'MSDS[0-9]+', --WRONG Syntax
 	   'MSDS691' SIMILAR TO 'MSDS[0-9]+',
 	   'MSDS691' SIMILAR TO '[a-z,0-9]+',
 	   'MSDS691' SIMILAR TO '[A-Z,0-9]+',
@@ -147,13 +166,16 @@ SELECT 'MSDS691' LIKE 'MSDS%',
 --------------------------------------------------------
 -- 4.A Return all the site name that includes “Fresno” in its name (case insensitive).
 --------------------------------------------------------
-
-
+select site_name
+from epa_site_location
+where lower(site_name) like '%fresno%';
 --------------------------------------------------------
 -- 4.B Return all the site name starting with ‘a’ (case insensitive) in ascending order.
 --------------------------------------------------------
-
-
+select site_name
+from epa_site_location
+where lower(site_name) like 'a%'
+order by site_name;
 ------------------------------------------------------------
 -- Mathematical Functions - p.31
 ------------------------------------------------------------
@@ -168,13 +190,16 @@ SELECT ROUND(42.4382, 2),
 --------------------------------------------------------
 -- Ex 5. Return  site_id and longitude/latitude rounded to the nearest two decimal places.
 --------------------------------------------------------
-
+select site_id, (site_longitude/site_latitude)::NUMERIC(100,2)
+from epa_site_location;
 
 
 --------------------------------------------------------
 -- Ex 6. Return uniqe date, its year/month and timestamp truncated to "month" in epa_air_quality ordered by date.
 --------------------------------------------------------
-
+SELECT DISTINCT date, extract(year from date) as year, extract(month from date) as month, date_trunc('month', date)
+from epa_air_quality
+order by date;
 
 --------------------------------------------------------
 -- Date Time Functions p.36
@@ -188,3 +213,12 @@ SELECT CURRENT_DATE,
 --------------------------------------------------------
 -- Ex 7. Return site_id that does not have any air quality records
 --------------------------------------------------------
+select site_id from epa_site_location
+except
+select site_id from epa_air_quality
+order by site_id;
+
+select distinct site_id
+from epa_site_location
+where site_id not in (select site_id from epa_air_quality)
+order by 1;
